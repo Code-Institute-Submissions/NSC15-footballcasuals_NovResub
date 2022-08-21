994baba66a8bce6e6a8fd497b2e5bafd9f8edec1
NSC15 <nathancave123@gmail.com>
2022-08-21 19:20:12 +0000

    add store app view and urls, create products.html, design site layout

--- casualsshop/urls.py
+++ casualsshop/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('accounts/', include('allauth.urls')),
+    path('products/', include('store.urls')),
 
 ]
