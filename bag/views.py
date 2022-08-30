b5ef09a47f2c3b48e0d851dcb9d8ce9c7541aeff
NSC15 <nathancave123@gmail.com>
2022-08-30 17:36:48 +0000

    created added products to bag function

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 def view_bag(request):
     return render(request, 'bag/bag.html')
+
+def add_to_bag(request, item_id):
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    return redirect(redirect_url)
--- casualsshop/settings.py
+++ casualsshop/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["footballcasuals.herokuapp.com", "localhost"]
 
@@ -92,7 +92,8 @@ WSGI_APPLICATION = 'casualsshop.wsgi.application'
 
 
 DATABASES = {
-   'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+   'default': 
+   dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 # Database
