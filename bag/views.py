05a7723a48e521495817ceaa57b7e4c6c0bae6a1
NSC15 <nathancave123@gmail.com>
2022-09-01 20:05:49 +0000

    add sizing to product model, create first structure to bag html page

--- bag/views.py
+++ bag/views.py
@@ -8,6 +8,9 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     bag = request.session.get('bag', {})
   
     if item_id in list(bag.keys()):
